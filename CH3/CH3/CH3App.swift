92cb73483cc95635691a048899a2bc2daf846e80
JavohirMX <javohirmx@gmail.com>
2026-05-28 19:14:14 +0800

    chore: update .gitignore

---
+++ CH3/CH3/CH3App.swift
@@ -0,0 +1,17 @@
+//
+//  CH3App.swift
+//  CH3
+//
+//  Created by Javohir Muhammad on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CH3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
